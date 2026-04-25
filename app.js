9364384e6aa7a416a8b3f43bf95b6db282cefd36
Eddzulla <eddi@flowneuroscience.com>
2026-04-25 16:08:37 +0200

    TAST

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
-const app = express(); // This is our server
-
 const authorRouter = require('./routes/authorRouter.js');
 const bookRouter = require('./routes/bookRouter.js')
-const indexRouter = require('./routes/indexRouter.js')
+const indexRouter = require('./routes/indexRouter.js');
+const CustomNotFoundError = require('./errors/CustomNotFoundError.js');
+
+const app = express(); // This is our server
+const PORT = 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +15,6 @@ app.use("/authors/", authorRouter);
 app.use("/books/", bookRouter);
 app.use("/", indexRouter)
 
-const PORT = 8000;
 // We tell the server which port to listen for GET-requests
 app.listen(PORT, (error) => {
     if (error) {
@@ -24,6 +24,10 @@ app.listen(PORT, (error) => {
     console.log(`http://localhost:${PORT}`);
 });
 
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(err.statusCode || 500).send(err);
+})
 
 
 
--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,16 +1,15 @@
 const db = require("../db.js");
+const CustomNotFoundError = require("../errors/CustomNotFoundError.js");
 
 async function getAuthorById(req, res){
     const { authorId } = req.params;
     console.log(req.params.authorId);
-
+    
     const author = await db.getAuthorById(Number(authorId));
 
     if(!author){
-        res.status(404).send("Author not found");
-        return;
+        throw new CustomNotFoundError("Author not found");
     }
-
     res.send(`Author name: ${author.name}`);
 };
 
